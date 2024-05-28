b81b5e2ca9687b4d07a0c8b5df29f544283c675d
simon <ghdud0503@gmail.com>
2024-05-28 16:14:19 +0900

    2-6

--- src/events/onData.js
+++ src/events/onData.js
@@ -3,6 +3,9 @@ import { PACKET_TYPE } from '../constants/header.js';
 import { packetParser } from '../utils/parser/packetParser.js';
 import { getHandlerById } from '../handlers/index.js';
 import { getUserById } from '../session/user.session.js';
+import { handleError } from '../utils/error/errorHandler.js';
+import CustomError from '../utils/error/customError.js';
+import { ErrorCodes } from '../utils/error/errorCodes.js';
 
 export const onData = (socket) => async (data) => {
   // 기존 버퍼에 새로 수신된 데이터를 추가
@@ -24,27 +27,28 @@ export const onData = (socket) => async (data) => {
       const packet = socket.buffer.slice(totalHeaderLength, totalHeaderLength + length);
       socket.buffer = socket.buffer.slice(totalHeaderLength + length);
 
-      console.log(`length: ${length}`);
-      console.log(`packetType: ${packetType}`);
+      try {
+        switch (packetType) {
+          case PACKET_TYPE.PING:
+            break;
+          case PACKET_TYPE.NORMAL:
+            const { handlerId, sequence, payload, userId } = packetParser(packet);
 
-      switch (packetType) {
-        case PACKET_TYPE.PING:
-          break;
-        case PACKET_TYPE.NORMAL:
-          const { handlerId, sequence, payload, userId } = packetParser(packet);
+            const user = getUserById(userId);
+            // 유저가 접속해 있는 상황에서 시퀀스 검증
+            if (user && user.sequence !== sequence) {
+              throw new CustomError(ErrorCodes.INVALID_SEQUENCE, '잘못된 호출 값입니다. ');
+            }
 
-          const user = getUserById(userId);
-          // 유저가 접속해 있는 상황에서 시퀀스 검증
-          if (user && user.sequence !== sequence) {
-            console.error('잘못된 호출 값입니다.');
-          }
-
-          const handler = getHandlerById(handlerId);
-          await handler({
-            socket,
-            userId,
-            payload,
-          });
+            const handler = getHandlerById(handlerId);
+            await handler({
+              socket,
+              userId,
+              payload,
+            });
+        }
+      } catch (error) {
+        handleError(socket, error);
       }
     } else {
       // 아직 전체 패킷이 도착하지 않음
