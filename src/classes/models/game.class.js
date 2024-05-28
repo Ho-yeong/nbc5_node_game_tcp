565007398630cfb5d8add5a4e5d42f38977d6fd1
simon <ghdud0503@gmail.com>
2024-05-29 01:34:18 +0900

    5-3

--- src/classes/models/game.class.js
+++ src/classes/models/game.class.js
@@ -1,4 +1,8 @@
 import IntervalManager from '../managers/interval.manager.js';
+import {
+  createLocationPacket,
+  gameStartNotification,
+} from '../../utils/notification/game.notification.js';
 
 const MAX_PLAYERS = 2;
 
@@ -37,8 +41,33 @@ class Game {
     }
   }
 
+  getMaxLatency() {
+    let maxLatency = 0;
+    this.users.forEach((user) => {
+      console.log(`${user.id}: ${user.latency}`);
+      maxLatency = Math.max(maxLatency, user.latency);
+    });
+    return maxLatency;
+  }
+
   startGame() {
     this.state = 'inProgress';
+    const startPacket = gameStartNotification(this.id, Date.now());
+    console.log(`max latency: ${this.getMaxLatency()}`);
+
+    this.users.forEach((user) => {
+      user.socket.write(startPacket);
+    });
+  }
+
+  getAllLocation() {
+    const maxLatency = this.getMaxLatency();
+
+    const locationData = this.users.map((user) => {
+      const { x, y } = user.calculatePosition(maxLatency);
+      return { id: user.id, x, y };
+    });
+    return createLocationPacket(locationData);
   }
 }
 
